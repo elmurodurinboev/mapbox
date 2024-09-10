c1a8878d5ea020fefc751690dd868bd4758b0fec
elmurodDev <elmurodorinboyev@gmail.com>
2024-09-10 15:02:50 +0500

    feat: Add route to the map

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,92 @@
-import Map, {FullscreenControl, GeolocateControl, Marker, NavigationControl, useControl} from 'react-map-gl';
-import MapboxDraw from '@mapbox/mapbox-gl-draw';
-import "./App.css"
+import Map, {GeolocateControl, Layer, Marker, NavigationControl, Source, useMap} from 'react-map-gl';
+import {useEffect, useState} from "react";
 
-function DrawControl(props) {
-    useControl(() => new MapboxDraw(props), {
-        position: props.position
-    });
+import mbxDirections from '@mapbox/mapbox-sdk/services/directions';
 
-    return null;
-}
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiZWxtdXJvZDgxMiIsImEiOiJjbHpzOWJ2YW0yM3ptMmxxdWFrcXVid2R2In0.PVkyuVTk9URyPdmvJQZLVw'; // Replace with your Mapbox token
+
+const directionsClient = mbxDirections({ accessToken: "pk.eyJ1IjoiZWxtdXJvZDgxMiIsImEiOiJjbHpzOWJ2YW0yM3ptMmxxdWFrcXVid2R2In0.PVkyuVTk9URyPdmvJQZLVw" });
 
 function App() {
-    return (
-        <Map
-            mapboxAccessToken="pk.eyJ1IjoiZWxtdXJvZDgxMiIsImEiOiJjbHpzOWJ2YW0yM3ptMmxxdWFrcXVid2R2In0.PVkyuVTk9URyPdmvJQZLVw"
-            initialViewState={{
-                longitude: 60.62035381793976,
-                latitude: 41.55811826919,
-                zoom: 18
-            }}
-            style={{width: '100%', height: '100vh', overflow: 'hidden'}}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-        >
-            <Marker longitude={60.62035381793976} latitude={41.55811826919}>
-                <img src={"./car-marker.png"} alt={"Car"} className={"car-marker"}/>
-            </Marker>
+  const [route, setRoute] = useState(null);  // State to hold the route data
+  const [viewport, setViewport] = useState({
+    latitude: 41.55811826,  // Center of the map (source location)
+    longitude: 60.6203538,
+    zoom: 14,
+    bearing: 0
+  });
 
-            <GeolocateControl position="top-right" />
-            <NavigationControl position="top-right" />
-        </Map>
-    );
+  // useEffect(() => {
+  //   const fetchCarRoute = async () => {
+  //     try {
+  //       const response = await directionsClient
+  //         .getDirections({
+  //           profile: 'driving',  // Use the "driving" profile for car routes
+  //           waypoints: [
+  //             { coordinates: [60.6203538, 41.55811826] }, // Source
+  //             { coordinates: [60.6056448, 41.5510189] },  // Destination
+  //           ],
+  //           geometries: 'geojson', // We want the geometry in GeoJSON format
+  //         })
+  //         .send();
+  //
+  //       const routeData = response.body.routes[0].geometry;
+  //       setRoute(routeData); // Set the fetched route data to state
+  //     } catch (error) {
+  //       console.error('Error fetching route: ', error);
+  //     }
+  //   };
+  //
+  //   fetchCarRoute();  // Fetch the route when the component mounts
+  // }, []);
+
+  useEffect(() => {
+    // Fetch route from OSRM
+    const fetchRoute = async () => {
+      const response = await fetch('https://router.project-osrm.org/route/v1/driving/60.6203538,41.55811826;60.6056448,41.5510189?geometries=geojson&overview=full');
+      const data = await response.json();
+      const routeData = data.routes[0].geometry;
+      setRoute(routeData);
+    };
+
+    fetchRoute();
+  }, []);
+
+  const routeLayer = {
+    id: 'route',
+    type: 'line',
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round',
+    },
+    paint: {
+      'line-color': '#ff7e5f',
+      'line-width': 4,
+    },
+  };
+
+  return (
+    <div>
+      <Map
+        {...viewport}
+        mapboxAccessToken={MAPBOX_TOKEN}
+        style={{width: '100%', height: '100vh', overflow: 'hidden'}}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onMove={(evt) => setViewport({...evt.viewState, bearing: 0})}
+      >
+        <Marker longitude={60.62035381793976} latitude={41.55811826919}>
+          <img src={"./car-marker.png"} alt={"Car"}/>
+        </Marker>
+        <NavigationControl position="bottom-right" showCompass={false}/>
+        <GeolocateControl position="bottom-right"/>
+        {route && (  // Render route only if it's fetched
+          <Source id="car-route" type="geojson" data={route}>
+            <Layer {...routeLayer} />
+          </Source>
+        )}
+      </Map>
+    </div>
+  );
 }
 
 export default App
